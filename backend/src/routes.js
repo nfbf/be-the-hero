2b3a730eb2fb5028c9dd2bb7e8caeb43610fa403
nfbf <45438873+nfbf@users.noreply.github.com>
2020-03-27 00:19:37 -0300

    criação de rotas do backend

--- backend/src/routes.js
+++ backend/src/routes.js
@@ -1,18 +1,22 @@
 const express = require('express');
 
+const OngController = require('./controllers/OngController')
+const IncidentController = require('./controllers/IncidentController')
+const ProfileController = require('./controllers/IncidentController')
+const SessionController = require('./controllers/IncidentController')
+
 const routes = express.Router();
 
 
-routes.post('/users', (request, response) => {
-    const body = request.body;
-   
-    console.log(body);
- 
-    return response.json({
-     evento: 'Semana OmniStack 11.0',
-     aluno:'Nicolly Figueiredo'
-    });
-});
+routes.post('/sessions', SessionController.create);
 
+routes.get('/ongs', OngController.index);
+routes.post('/ongs', OngController.create);
+
+routes.get('/profile', ProfileController.index);
+
+routes.get('/incidents', IncidentController.index);
+routes.post('/incidents', IncidentController.create);
+routes.delete('/incidents/:id', IncidentController.delete);
 
 module.exports = routes;
